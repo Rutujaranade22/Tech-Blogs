61334a602b9e4bf76db6debb20945d6d5faea163
Rutujaranade22 <ranaderutuja7@gmail.com>
2025-09-27 16:52:46 +0530

    markdown lib install and used

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,17 +1,29 @@
-import User from "../models/User.js";
+import md5 from "md5";
+import User from "./../models/User.js";
 
-const postSignup =async (req, res)=>{
- const {name,email,password}= req.body;
+const postSignup = async (req, res) => {
+   const newUser = new User({ name, email, password: md5(password) });
 
- const newUser = new User ({name, email,password});
+  const savedUser = await newUser.save();
 
- const savedUser = await newUser.save();
-res.json({
-    success:true,
-    message:"User register successfully",
-    user:savedUser,
-});
+
+   const existingUser = await User.findOne({
+    email,
+    password: md5(password),
+  }).select("_id name email");
+
+  if (existingUser) {
+    return res.json({
+      success: true,
+      message: "User logged in successfully",
+      user: existingUser,
+    });
+  } else {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid email or password",
+    });
+  }
 };
-const postLogin = (req, res)=>{};
 
-export {postLogin,postSignup};
+export { postLogin, postSignup };
